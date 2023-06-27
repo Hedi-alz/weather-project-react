e06fce9f6312aaf7f4feabb6a12a22091ff12604
Hedi-alz <hedieh.kh.alizadeh@gmail.com>
2023-06-28 02:23:45 +0430

    added Github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      weather app
+      <p>
+        this project is created bye <em>Hedieh</em> ans is{" "}
+        <a
+          href="https://github.com/Hedi-alz/weather-project-react"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </p>
+    </div>
+  );
 }
