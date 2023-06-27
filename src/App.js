f3fccf3748873bbd4cef1bd83538f983b2afd561
Hedi-alz <hedieh.kh.alizadeh@gmail.com>
2023-06-28 02:41:18 +0430

    import Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      weather app
-      <p>
-        this project is created bye <em>Hedieh</em> ans is{" "}
-        <a
-          href="https://github.com/Hedi-alz/weather-project-react"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </p>
+      <div className="container">
+        weather app
+        <p>
+          this project is created bye <em>Hedieh</em> ans is{" "}
+          <a
+            href="https://github.com/Hedi-alz/weather-project-react"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
