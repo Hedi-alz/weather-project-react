93a34ddc1b06c451d405929c0fb3799a6f572d6a
Hedi-alz <hedieh.kh.alizadeh@gmail.com>
2023-06-28 04:33:14 +0430

    styled the content

--- src/Weather.js
+++ src/Weather.js
@@ -5,24 +5,41 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="search for a city"
-          className="formContorl"
-        />
-        <input type="submit" value="search" className="buttom" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="search for a city"
+              className="form-contorl"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>New York</h1>
       <ul>
         <li>Wednesday 7:00</li>
         <li>Rainy</li>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="weather icon"
-            />
-            6°C
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="weather icon"
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="temperature">6</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
           </div>
           <div className="col-6">
             <ul>
